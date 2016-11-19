2f51ae0b040d8b21c8ab295de307ccf6b6f93165
Каратаев Алексей <hol0d@me.com>
2016-11-19 09:52:30 +0300

    Implementation generic management methods

--- HackatonApp/Database/Database.swift
+++ HackatonApp/Database/Database.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import CoreData
+
+class Database {
+    static let instance = Database()
+    
+    
+    func getEntities<T: NSManagedObject>() -> [T]? {
+        let request: NSFetchRequest<T> = NSFetchRequest(
+            entityName: T.getEntityName())
+        do {
+            let entities = try CoreDataManager.instance
+                .managedObjectContext.fetch(request)
+            return entities
+        } catch (let error as NSError) {
+            print(error.description)
+        }
+        return nil
+    }
+    
+    func getEntities<T: NSManagedObject>(
+        predicate: NSPredicate) -> [T]? {
+        let request: NSFetchRequest<T> = NSFetchRequest(
+            entityName: T.getEntityName())
+        request.predicate = predicate
+        do {
+            let entities = try CoreDataManager.instance
+                .managedObjectContext.fetch(request)
+            return entities
+        } catch (let error as NSError) {
+            print(error.description)
+        }
+        return nil
+    }
+    
+    func deleteEntities<T: NSManagedObject>(entities: [T]) {
+        for entity in entities {
+            CoreDataManager.instance.managedObjectContext.delete(entity)
+        }
+    }
+}
